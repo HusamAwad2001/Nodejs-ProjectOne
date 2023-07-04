5e262c6555c3f9829a71e2c31726859252e11c3d
HusamAwad2001 <www.hosamawad2016@gmail.com>
2023-07-04 14:54:22 +0300

    create request and test it with postman

--- server.js
+++ server.js
@@ -3,6 +3,25 @@ const express = require('express');
 // inhereted all functionality on express
 const app = express();
 
+
+
+app.get('/getall', ()=>{
+    console.log('Get work');
+});
+
+app.post('/add', ()=>{
+    console.log('Add work');
+});
+
+app.put('/update', ()=>{
+    console.log('Update work');
+});
+
+app.delete('/delete', ()=>{
+    console.log('Delete work');
+});
+
+
 app.listen(3000, ()=>{
     console.log('Server is running!');
 });
